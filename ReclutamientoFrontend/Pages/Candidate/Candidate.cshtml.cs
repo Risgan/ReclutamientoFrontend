e2af2793e55fd343f775c9396541589b5d84359f
John Rueda <risgan2727@gmail.com>
2023-10-16 18:26:39 -0500

    Agregar archivos de proyecto.

---
+++ ReclutamientoFrontend/Pages/Candidate/Candidate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ReclutamientoFrontend.Pages.Candidate
+{
+    public class CandidateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
